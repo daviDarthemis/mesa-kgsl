a59af66cab4c7e6d9c49dd1c67709bb1a758a2b3
Davi D'artemis <99675770+daviDarthemis@users.noreply.github.com>
2024-05-27 14:22:49 -0300

    debug

--- src/gallium/drivers/panfrost/pan_screen.c
+++ src/gallium/drivers/panfrost/pan_screen.c
@@ -919,15 +919,23 @@ panfrost_create_screen(int fd, const struct pipe_screen_config *config,
 struct pipe_screen *
 panfrost_create_screen_sw(struct sw_winsys *winsys)
 {
-        int fd = drmOpenWithType("panfrost", NULL, DRM_NODE_RENDER);
-        if (fd < 0)
-                fd = open("/dev/mali0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
-        if (fd < 0)
-                return NULL;
+   int fd = drmOpenWithType("panfrost", NULL, DRM_NODE_RENDER);
 
-        struct pipe_screen *scr = panfrost_create_screen(fd ,0, NULL);
+   if (fd < 0)
+   {
+      fd = open("/dev/mali0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
+   }
+   else
+   {
+      fprint("dispositivo /dev/mali0 nao foi encontrado");
+      
+      return NULL;
+   }
 
-        if (scr)
-                pan_screen(scr)->sw_winsys = winsys;
-        return scr;
+   truct pipe_screen *scr = panfrost_create_screen(fd ,0, NULL);
+
+   if (scr)
+      pan_screen(scr)->sw_winsys = winsys;
+
+  return scr;
 }
